f48cbc665133ee1b1ee57111b23bc6f9f73f8820
Wiktoria Marzec <wiktoria.m.marzec@gmail.com>
2024-01-08 10:48:28 +0100

    file_comparator in progress

--- file_comparator.py
+++ file_comparator.py
@@ -2,22 +2,26 @@ def get_part (string):
     line = string.split(";")
     return (str(line[0]))
 
+def is_on_the_list (list, string):
+    for line in list:
+        line = get_part(line)
+        print (line)
+        print (string)
+        if string == line:
+            return True
+
 def get_file (list1, list2):
     list_for_file = []
     for j in list1:
         first_string = get_part(j)
         for k in list2:
             second_string = get_part(k)
-            print(first_string)
-            print(second_string)
             if first_string == second_string:
                 j = k
                 list_for_file.append(j)
-            #else:
-                #list_for_file.append(j)
-            print (list_for_file)
-    return list_for_file
-                
+        if is_on_the_list (list_for_file, first_string) != True:
+            list_for_file.append(j)
+    return list_for_file 
 
 def get_list (text):
     lines = []
@@ -31,6 +35,8 @@ old_file = get_list(r"old_file.txt")
 new_file = get_list(r"new_file.txt")
 
 fixed_file = get_file(old_file, new_file)
-#print (fixed_file)
+logs = []
+print (fixed_file)
+
 
 
