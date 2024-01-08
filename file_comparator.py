2a081e7dc860a8197f5821e136dbe4a1b9e512fe
Wiktoria Marzec <wiktoria.m.marzec@gmail.com>
2024-01-08 11:07:21 +0100

    file_comparator

--- file_comparator.py
+++ file_comparator.py
@@ -5,20 +5,18 @@ def get_part (string):
 def is_on_the_list (list, string):
     for line in list:
         line = get_part(line)
-        print (line)
-        print (string)
         if string == line:
             return True
 
-def get_file (list1, list2):
+def get_file (list1, list2, logs):
     list_for_file = []
     for j in list1:
         first_string = get_part(j)
         for k in list2:
             second_string = get_part(k)
             if first_string == second_string:
-                j = k
-                list_for_file.append(j)
+                list_for_file.append(k)
+                logs.append(j + " > " + k)
         if is_on_the_list (list_for_file, first_string) != True:
             list_for_file.append(j)
     return list_for_file 
@@ -31,12 +29,18 @@ def get_list (text):
             lines.append(line) 
     return lines
 
-old_file = get_list(r"old_file.txt")
-new_file = get_list(r"new_file.txt")
-
-fixed_file = get_file(old_file, new_file)
+old_file = get_list(r"old file.txt")
+new_file = get_list(r"new file.txt")
 logs = []
-print (fixed_file)
+fixed_file = get_file(old_file, new_file, logs)
+
+with open(r'file from script.txt', 'x') as fp:
+    for item in fixed_file:
+        fp.write("%s\n" % item)
+
+with open(r'logs.txt', 'x') as fp:
+    for item in logs:
+        fp.write("%s\n" % item)
 
 
 
